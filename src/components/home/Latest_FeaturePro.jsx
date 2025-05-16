dba7420637fbd00bd6a88935d87ff6c64eda6dab
arafat709 <mdarafat112007@gmail.com>
2025-05-16 21:43:24 +0600

    again pathName updated

--- src/components/home/Latest_FeaturePro.jsx
+++ src/components/home/Latest_FeaturePro.jsx
@@ -22,7 +22,7 @@ const Latest_FeaturePro = () => {
     >
       <div className="container mx-auto h-full">
         {/* main container */}
-      <div className="flex flex-col lg:flex-row justify-between items-center h-full relative">
+        <div className="flex flex-col lg:flex-row justify-between items-center h-full relative">
           {/* Left side-Image container */}
           <div className="lg:w-1/2 w-full h-full flex items-center justify-center relative">
             <div className="absolute inset-0 flex items-center justify-center z-0">
@@ -42,7 +42,9 @@ const Latest_FeaturePro = () => {
           </div>
           {/* Right side */}
           <div className="lg:w-1/2 w-full lg:pl-8 pl-0 lg:mt-0 mt-8">
-            <h2 className="text-[35px] font-bold text-[#151875] mb-6">Unique Features Of leatest & Trending Poducts</h2>
+            <h2 className="text-[35px] font-bold text-[#151875] mb-6">
+              Unique Features Of leatest & Trending Poducts
+            </h2>
             <ul className="space-y-4 text-[#ACABC3] font-semibold mb-8">
               <li className="flex items-start">
                 <span className="w-3 h-3 rounded-full bg-pink-500 mr-2 mt-1 flex-shrink-0"></span>
@@ -50,7 +52,10 @@ const Latest_FeaturePro = () => {
               </li>
               <li className="flex items-start">
                 <span className="w-3 h-3 rounded-full bg-blue-600 mr-2 mt-1 flex-shrink-0"></span>
-                <p>Reinforced with double wood dowels, glue, screw - nails corner blocks and machine nails</p>
+                <p>
+                  Reinforced with double wood dowels, glue, screw - nails corner
+                  blocks and machine nails
+                </p>
               </li>
               <li className="flex items-start">
                 <span className="w-3 h-3 rounded-full bg-teal-400 mr-2 mt-1 flex-shrink-0"></span>
@@ -58,7 +63,9 @@ const Latest_FeaturePro = () => {
               </li>
             </ul>
             <div className="flex items-center gap-8">
-                <button className="bg-[#FB2E86] text-white px-6 py-2 rounded-md hover:bg-[#e02678] transition cursor-pointer font-semibold">Add To Cart</button>
+              <button className="bg-[#FB2E86] text-white px-6 py-2 rounded-md hover:bg-[#e02678] transition cursor-pointer font-semibold">
+                Add To Cart
+              </button>
               <div className="text-[#151875]">
                 <h4 className="font-bold">B&B Italian Sofa </h4>
                 <h6 className="font-semibold">$100.00</h6>
